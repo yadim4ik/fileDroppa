0fc5f8b8d6614dcae0d6f1e14dab15873f65675a
WorkPashtet <work.pashtet@gmail.com>
2016-04-19 13:00:22 -0400

    Updated dependencies

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+///<reference path="node_modules/typescript/lib/lib.es6.d.ts" />
+
 export * from "./components/Directives/FileDropZone";
 export * from "./components/Directives/FileDroppa";
 export * from "./components/Directives/FileList";
