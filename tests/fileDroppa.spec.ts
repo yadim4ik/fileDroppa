1b8f277632fca3ae754858caa4fe9eb179f91711
WorkPashtet <work.pashtet@gmail.com>
2016-06-22 19:48:26 -0400

    autoUpload covered with test

--- tests/fileDroppa.spec.ts
+++ tests/fileDroppa.spec.ts
@@ -4,6 +4,7 @@ import {
     describe,
     expect,
     inject,
+    async,
     beforeEachProviders,
     it
 } from '@angular/core/testing';
@@ -15,8 +16,8 @@ import {FileDropZone} from "../components/Directives/FileDropZone";
 import {FileList} from "../components/Directives/FileList";
 
 const createFile = ()=> {
-    return new Blob([["<!doctype html><div>File</div>"]], {type:"text/html"});
-
+    var blob = new Blob([["<!doctype html><div>File</div>"]], {type:"text/html"});
+    return new File([blob], 'blob.html');
 };
 
 @Component({
@@ -26,111 +27,158 @@ const createFile = ()=> {
         <fileDroppa></fileDroppa>
     `
 })
+class TestComponentNoInput {
+}
+
+@Component({
+    selector: 'app-component',
+    directives: [FileDroppa],
+    template: `
+        <fileDroppa
+                    [url]="'https://salty-taiga-80701.herokuapp.com/upload'"
+                    [beforeRequest]="beforeRequest"
+                    [beforeFileUpload]="beforeFileUpload"
+                    [beforeAddFile]="beforeAddFile"
+                    (filesUpdated)="filesUpdated($event)"
+                    (fileUploaded)="fileUploaded($event)"
+        ></fileDroppa>
+    `
+})
 class TestComponent {
+    public fileUploadedCalled = false;
+    public filesUpdatedCalled = false;
+    public beforeRequstCalled = false;
+    public beforeFileUploadCalled = false;
+    public beforeAddFileCalled = false;
+
+    constructor(){
+        this.fileUploaded = this.fileUploaded.bind(this);
+        this.filesUpdated = this.filesUpdated.bind(this);
+        this.beforeRequest = this.beforeRequest.bind(this);
+        this.beforeFileUpload = this.beforeFileUpload.bind(this);
+        this.beforeAddFile = this.beforeAddFile.bind(this);
+    }
+    fileUploaded(success, response, file){
+        this.fileUploadedCalled = true;
+    }
+
+    filesUpdated(files) {
+        this.filesUpdatedCalled = true;
+    }
+
+    beforeRequest(xhr){
+        this.beforeRequstCalled = true;
+    }
+
+    beforeFileUpload(formData){
+        this.beforeFileUploadCalled = true;
+        return formData;
+    }
+
+    beforeAddFile(file){
+        this.beforeAddFileCalled = true;
+        return true;
+    }
 }
 
 describe('FileDroppa', () => {
 
-    let builder: TestComponentBuilder;
-    //let fixture: ComponentFixture<TestComponent>;
+    let builder: TestComponentBuilder,
+        fileDroppaComponent,
+        fileAddedPromise,
+        fileDropZone;
+
+    let fixture: ComponentFixture<TestComponent>;
 
     beforeEach(inject([TestComponentBuilder], (tcb) => {
         builder = tcb;
     }));
 
-    it('should render', done => {
-        builder.createAsync(TestComponent).then(f => {
+    describe("testing INPUT parameters", ()=>{
+        beforeEach(async(() => {
+            builder.createAsync(TestComponent).then(f => {
+                fixture = f;
+                fileDroppaComponent = fixture.debugElement.query(By.directive(FileDroppa)).componentInstance;
+                fileDropZone = fixture.debugElement.query(By.directive(FileDropZone));
+                fixture.detectChanges();
+
+                let event = <any>document.createEvent('MouseEvent');
+                event.initEvent('drop', true, true);
+                event.dataTransfer = {
+                    files:[createFile()]
+                };
+                fileDropZone.nativeElement.dispatchEvent(event);
+                fileAddedPromise = fixture.debugElement.query(By.directive(FileDropZone)).componentInstance.promise;
+            });
+        }));
+
+        it('should render', () => {
             expect(document.querySelectorAll('.file-droppa-container').length).toBe(1);
-            done();
         });
-    });
 
-    it('adds file on drop', (done) => {
-        builder.createAsync(TestComponent)
-            .then(fixture => {
-                let fileDroppaComponent = fixture.debugElement.query(By.directive(FileDroppa)).componentInstance;
-                let fileDropZone = fixture.debugElement.query(By.directive(FileDropZone));
-                fixture.detectChanges();
-                let event = <any>document.createEvent('MouseEvent');
-                event.initEvent('drop', true, true);
-                event.dataTransfer = {
-                    files:[createFile()]
-                };
-                fileDropZone.nativeElement.dispatchEvent(event);
-                fixture.detectChanges();
-                fixture.debugElement.query(By.directive(FileDropZone)).componentInstance.promise.then(()=>{
-                    expect(fileDroppaComponent.filesStore.files.length).toBe(1);
-                    done();
-                });
+        it('adds file on drop', (done) => {
+            fileAddedPromise.then(()=>{
+                expect(fileDroppaComponent.filesStore.files.length).toBe(1);
+                done();
             });
-    });
+        });
 
-    it('renders list', (done) => {
-        builder.createAsync(TestComponent)
-            .then(fixture => {
-                let fileDropZone = fixture.debugElement.query(By.directive(FileDropZone));
-                let event = <any>document.createEvent('MouseEvent');
-                event.initEvent('drop', true, true);
-                event.dataTransfer = {
-                    files:[createFile()]
-                };
-                fileDropZone.nativeElement.dispatchEvent(event);
-                fixture.detectChanges();
-                fixture.debugElement.query(By.directive(FileDropZone)).componentInstance.promise.then(()=>{
-                    expect(fixture.debugElement.query(By.directive(FileList))).toBeTruthy();
-                    done();
-                });
+        it('renders files list', (done) => {
+            fileAddedPromise.then(()=>{
+                expect(fixture.debugElement.query(By.directive(FileList))).toBeTruthy();
+                done();
             });
-    });
+        });
 
-    it('doesnt render list when it diabled in config', (done) => {
-        builder.createAsync(TestComponent)
-            .then(fixture => {
-                let fileDroppaComponent = fixture.debugElement.query(By.directive(FileDroppa)).componentInstance;
-                fileDroppaComponent.showFilesList = false;
-                let fileDropZone = fixture.debugElement.query(By.directive(FileDropZone));
-                fixture.detectChanges();
-                let event = <any>document.createEvent('MouseEvent');
-                event.initEvent('drop', true, true);
-                event.dataTransfer = {
-                    files:[createFile()]
-                };
-                fileDropZone.nativeElement.dispatchEvent(event);
-                fixture.detectChanges();
-                fixture.debugElement.query(By.directive(FileDropZone)).componentInstance.promise.then(()=>{
-                    expect(fixture.debugElement.query(By.directive(FileList))).toBeFalsy();
-                    done();
-                });
+        it('doesnt render list when it diabled in config', (done) => {
+            fileDroppaComponent.showFilesList = false;
+            fixture.detectChanges();
+            fileAddedPromise.then(()=>{
+                expect(fixture.debugElement.query(By.directive(FileList))).toBeFalsy();
+                done();
             });
-    });
+        });
 
 
-    it('throws if no url given', (done) => {
-        builder.createAsync(TestComponent)
-            .then(fixture => {
-                let fileDroppaComponent = fixture.debugElement.query(By.directive(FileDroppa)).componentInstance;
-                let fileDropZone = fixture.debugElement.query(By.directive(FileDropZone));
-                fixture.detectChanges();
-                let event = <any>document.createEvent('MouseEvent');
-                event.initEvent('drop', true, true);
-                event.dataTransfer = {
-                    files:[createFile()]
-                };
-                fileDropZone.nativeElement.dispatchEvent(event);
-                fixture.detectChanges();
-                fixture.debugElement.query(By.directive(FileDropZone)).componentInstance.promise.then(()=>{
-                    expect(()=>{
-                        fileDroppaComponent.uploadAllFiles()
-                    }).toThrow();
-                    done();
+        it('throws if no url given', (done) => {
+            builder.createAsync(TestComponentNoInput)
+                .then(fixture => {
+                    let fileDroppaComponent = fixture.debugElement.query(By.directive(FileDroppa)).componentInstance;
+                    let fileDropZone = fixture.debugElement.query(By.directive(FileDropZone));
+                    fixture.detectChanges();
+                    let event = <any>document.createEvent('MouseEvent');
+                    event.initEvent('drop', true, true);
+                    event.dataTransfer = {
+                        files:[createFile()]
+                    };
+                    fileDropZone.nativeElement.dispatchEvent(event);
+                    fixture.detectChanges();
+                    fixture.debugElement.query(By.directive(FileDropZone)).componentInstance.promise.then(()=>{
+                        expect(()=>{
+                            fileDroppaComponent.uploadAllFiles()
+                        }).toThrow();
+                        done();
+                    });
                 });
+        });
+
+        it('enables autoUpload', (done) => {
+            fileDroppaComponent.autoUpload = true;
+            fileDroppaComponent.showFilesList = false;
+            fixture.detectChanges();
+            let event = <any>document.createEvent('MouseEvent');
+            event.initEvent('drop', true, true);
+            event.dataTransfer = {
+                files:[createFile()]
+            };
+            fileDropZone.nativeElement.dispatchEvent(event);
+            fileAddedPromise.then(()=>{
+                expect(fixture.debugElement.componentInstance.beforeFileUploadCalled).toBe(true);
+                done();
             });
+        });
+
     });
-
-
-    //it('enables autoUpload', (done) => {
-        //TODO: IMPLEMENT!!
-    //});
 
 });
 
